ce79a09883f4cbfa2036ddba6f3b065bad8ddb32
ilya shusterman <shusterilyaman@gmail.com>
2018-01-19 08:57:56 +0200

    added big dataset of medical usage

--- predict_treatment_machine/TreatmentMachine.py
+++ predict_treatment_machine/TreatmentMachine.py
@@ -18,10 +18,12 @@ def get_input_diagnosis():
 
 
 def main():
-    treatment_df = pd.read_csv('people_medicine_data.csv')
+    treatment_df = pd.read_csv('people_medicine_data_large.csv')
     treatment_df = remove_useless_columns(treatment_df)
 
     common_treatment = set.intersection(set(treatment_df['treatment']))
+    # common_diagnose = set.intersection(set(treatment_df['diagnosis']))
+    # print(common_diagnose)
     treatment_dict = {treatment: i
                       for i, treatment in
                       zip(np.arange(len(common_treatment)),
@@ -34,22 +36,9 @@ def main():
                                                         random_state=42)
     feature_columns_list, input_function, label_y = get_feature_columns_and_input_function(
         treatment_df, treatment_dict, x_train, y_train)
-    print('values={} len={}'.format(treatment_dict.keys(), len(label_y)))
-    model = tf.estimator.LinearClassifier(
-        feature_columns=feature_columns_list, n_classes=len(label_y)+1)
-
-    model.train(input_fn=input_function, steps=5000)
-
-    prediction_function = tf.estimator.inputs.pandas_input_fn(x=x_test,
-                                                              batch_size=
-                                                              x_test.shape[
-                                                                  0],
-                                                              shuffle=False)
-    predictions = list(model.predict(input_fn=prediction_function))
-    predictions = [pred['class_ids'][0] for pred in predictions]
-    print(classification_report(y_test, predictions))
+    model = get_trained_model(feature_columns_list, input_function, label_y,
+                              treatment_dict, x_test, y_test)
     question_data = get_input_diagnosis()
-    # print (question_data)
     x_test_question = pd.DataFrame([question_data])
     prediction_function_question = \
         tf.estimator.inputs.pandas_input_fn(x=x_test_question,
@@ -61,6 +50,23 @@ def main():
     predictions = [res_treatment_dict[pred['class_ids'][0]]
                    for pred in predictions]
     print(predictions)
+
+
+def get_trained_model(feature_columns_list, input_function, label_y,
+                      treatment_dict, x_test, y_test):
+    print('values={} len={}'.format(treatment_dict.keys(), len(label_y)))
+    model = tf.estimator.LinearClassifier(
+        feature_columns=feature_columns_list, n_classes=len(label_y) + 1)
+    model.train(input_fn=input_function, steps=5000)
+    prediction_function = tf.estimator.inputs.pandas_input_fn(x=x_test,
+                                                              batch_size=
+                                                              x_test.shape[
+                                                                  0],
+                                                              shuffle=False)
+    predictions = list(model.predict(input_fn=prediction_function))
+    predictions = [pred['class_ids'][0] for pred in predictions]
+    print(classification_report(y_test, predictions))
+    return model
 
 
 def get_feature_columns_and_input_function(treatment_df, treatment_dict,
@@ -102,7 +108,7 @@ def get_data_and_labels(treatment_df):
 
 
 def remove_useless_columns(treatment_df):
-    return treatment_df.drop(['person_id', 'company'], axis=1)
+    return treatment_df.drop(['person_instance', 'company'], axis=1)
 
 
 if __name__ == '__main__':
